a9a45e4d8379440f7bc6fe7cd0e8046f380e5d1d
Michael Oyibo <codingrealms@gmail.com>
2017-06-03 11:56:52 +0100

    added startup file and Node class

--- Node.py
+++ Node.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-This is a pyMillionair quiz application
-"""
 import datetime
 
 __author__ = "Michael Oyibo"
@@ -15,4 +12,44 @@ __status__ = "Development"
 
 
 class Node(object):
-    def __init__(self, )
+    def __init__(self, myIP, myMAC, nodeList):
+        self.myIP = myIP
+        self.myMAC = myMAC
+        self.nodeList = nodeList
+        self.masterNode = ''
+
+    def add_node(self):
+        #Add newly added node
+        pass
+
+
+    def remove_node(self):
+        #Remove node from node that is no longer active
+        pass
+
+    def update_master_node(self, masterOctet):
+        if self.myIP.split('.')[3] == str(masterOctet):
+            self.masterNode = [self.myIP, self.myMAC.strip(), 'Raspberry Pi Foundation']
+            # print("I am the master!! {}".format(self.masterNode))
+        else:
+            masterNode = list(filter(lambda x: x[0][0].split('.')[3] == str(masterOctet), self.nodeList))
+            self.masterNode = masterNode
+            # print("Master node is {}".format(masterNode))
+
+    def fetch_master_node(self):
+        lastOctetOfPis = list(map(lambda x: x[0].split('.')[3], self.nodeList))
+        lastOctetOfPis.append(self.myIP.split('.')[3])
+
+        # print("Last octet of all node are {}".format(lastOctetOfPis))
+        #Node selection for master happens here
+
+        self.update_master_node(max(list(map(int, lastOctetOfPis))))
+
+        # print("Highest IP is  {}".format(max(list(map(int, lastOctetOfPis)))))
+
+    def getIPAddress(self):
+        return self.myIP
+
+    def getMACAddress(self):
+        return self.myMAC
+
--- start.py
+++ start.py
@@ -7,17 +7,23 @@ import time
 import re
 from Node import *
 
+__author__ = "Michael Oyibo"
+__copyright__ = "Copyright 2017, Open Computing Enterprise Course"
+__credits__ = ["Michael Oyibo"]
+__version__ = "1.0.1"
+__maintainer__ = "Michael Oyibo"
+__email__ = "wi13b051@technikum-wien.at"
+__status__ = "Development"
 
-try:
-
+def initialize():
     nmap3 = subprocess.check_output("sudo nmap -sP 192.168.1.0/24", shell=True).decode('utf-8')
 
     macAddress = subprocess.check_output("ifconfig | awk '/eth0/ { print toupper($5) }'", shell=True).decode('utf-8')
 
     ipAddress = re.findall("([\d]+.[\d]+.[\d]+.[\d]+)",subprocess.check_output("hostname -I", shell=True).decode('utf-8'))[0]
 
-    print("This Node IP address is {} and MAC: {}".format(ipAddress, macAddress))
-    
+    # print("This Node IP address is {} and MAC: {}".format(ipAddress, macAddress))
+
     matches = re.findall("([\d]+.[\d]+.[\d]+.[\d]+)\nHost is up .*.\nMAC Address: ([0-9A-F:.]*) \((.*)\)\n",nmap3)
 
     listOfActivePIs = list(filter(lambda x: x[1].startswith('B8:27:EB'), matches))
@@ -25,8 +31,6 @@ try:
 
     thisNode = Node(ipAddress, macAddress, listOfActivePIs)
     thisNode.fetch_master_node();
-
-
-except Exception  as e:
-    print('nmam error')
-    print("{}".format(str(e)))
+    
+if __name__== '__main__':
+    initialize()
